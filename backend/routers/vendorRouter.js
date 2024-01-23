1bacf6b1049cb52d3ade3a0a32601c44b9798170
kiran-bsv <boddepallisai@iitbhilai.ac.in>
2024-01-23 18:15:44 +0530

    vendor registration succeeded

---
+++ backend/routers/vendorRouter.js
@@ -0,0 +1,100 @@
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import Vendor from "../models/vendorModel.js";
+import bcrypt from "bcryptjs";
+import { generateToken, isAuth } from "../utils.js";
+
+const vendorRouter = express.Router();
+
+vendorRouter.get("/seed", async (req, res) => {
+  // Assuming you have a vendors array for seeding
+  const createdVendors = await Vendor.insertMany(vendors);
+  res.send({ createdVendors });
+});
+
+vendorRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const vendor = await Vendor.findOne({ email: req.body.email });
+
+    if (vendor) {
+      if (bcrypt.compareSync(req.body.password, vendor.password)) {
+        res.send({
+          _id: vendor._id,
+          name: vendor.name,
+          email: vendor.email,
+          isAdmin: vendor.isAdmin,
+          token: generateToken(vendor),
+        });
+        return;
+      }
+    }
+
+    res.status(401).send({ message: "Invalid email or password." });
+  })
+);
+
+vendorRouter.post(
+  "/registerVendor",
+  expressAsyncHandler(async (req, res) => {
+    const {
+      name,
+      email,
+      password,
+      address,
+      phoneNumber,
+      shopName,
+      shopAddress,
+      shopLocation,
+      businessTypes,
+    } = req.body;
+    const vendor = new Vendor({
+      name,
+      email,
+      password: bcrypt.hashSync(password, 8),
+      address,
+      phoneNumber,
+      shopName,
+      shopAddress,
+      shopLocation,
+      businessTypes,
+    });
+
+    try {
+      const createdVendor = await vendor.save();
+      res.status(201).send({
+        _id: createdVendor._id,
+        name: createdVendor.name,
+        email: createdVendor.email,
+        isAdmin: createdVendor.isAdmin,
+        token: generateToken(createdVendor),
+      });
+    } catch (error) {
+      // Handle validation errors
+      if (error.name === "ValidationError") {
+        const errors = Object.values(error.errors).map((err) => err.message);
+        res.status(400).json({ message: errors.join(", ") });
+      } else {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    }
+  }) 
+);
+
+vendorRouter.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const vendor = await Vendor.findById(req.params.id);
+
+    if (vendor) {
+      res.send(vendor);
+    } else {
+      res.status(404).send({
+        message: "Vendor not found",
+      });
+    }
+  })
+);
+
+export default vendorRouter;
---
+++ frontend/src/Store.js
@@ -0,0 +1,47 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import { cartReducer } from './reducers/CartReducer';
+import { orderCreateReducer, orderDetailsReducer, orderMineListReducer, orderPayReducer } from './reducers/OrderReducer';
+import { prodcutDetailsReducer, prodcutListReducer } from './reducers/ProductReducer';
+import { userDetailsReducer, userRegisterReducer, userSigninReducer, userUpdateProfileReducer, vendorRegisterReducer, vendorSigninReducer } from './reducers/UserReducer';
+
+const initialState = {
+    cart:{
+        cartItems: localStorage.getItem('cartItems')
+         ? JSON.parse(localStorage.getItem('cartItems'))
+         : [],
+        shippingAddress: localStorage.getItem('shippingAddress')
+         ? JSON.parse(localStorage.getItem('shippingAddress'))
+         : {},
+        paymentMethod: 'PayPal',
+    },
+    userSignin:{
+        userInfo: localStorage.getItem('userInfo')
+         ? JSON.parse(localStorage.getItem('userInfo'))
+         : null,
+    }
+};
+const reducer = combineReducers({
+    productList: prodcutListReducer,
+    productDetails: prodcutDetailsReducer,
+    cart: cartReducer,
+    userRegister: userRegisterReducer,
+    userSignin: userSigninReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+    orderMineList: orderMineListReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile : userUpdateProfileReducer,
+    vendorRegister: vendorRegisterReducer,
+    vendorSignin: vendorSigninReducer
+});
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducer, 
+    initialState,
+    composeEnhancer(applyMiddleware(thunk)),
+);
+
+export default store;
